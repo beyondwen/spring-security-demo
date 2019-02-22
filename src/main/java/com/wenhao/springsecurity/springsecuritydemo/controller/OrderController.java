d7bf52857044e8c606e06e0894378675f0af4051
wenha0 <921790476@qq.com>
2019-02-22 20:48:19 +0800

    Initial commit

---
+++ src/main/java/com/wenhao/springsecurity/springsecuritydemo/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.wenhao.springsecurity.springsecuritydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OrderController {
+	// 首页
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	// 查询订单
+	@RequestMapping("/showOrder")
+	public String showOrder() {
+		return "showOrder";
+	}
+
+	// 添加订单
+	@RequestMapping("/addOrder")
+	public String addOrder() {
+		return "addOrder";
+	}
+
+	// 修改订单
+	@RequestMapping("/updateOrder")
+	public String updateOrder() {
+		return "updateOrder";
+	}
+
+	// 删除订单
+	@RequestMapping("/deleteOrder")
+	public String deleteOrder() {
+		return "deleteOrder";
+	}
+
+}
