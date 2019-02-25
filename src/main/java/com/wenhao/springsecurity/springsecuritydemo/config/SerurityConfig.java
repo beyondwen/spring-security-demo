e74cf67ee6a0a2c07676ea8c736ad5895b17c25c
wenha0 <921790476@qq.com>
2019-02-25 21:59:49 +0800

    认证授权

--- src/main/java/com/wenhao/springsecurity/springsecuritydemo/config/SerurityConfig.java
+++ src/main/java/com/wenhao/springsecurity/springsecuritydemo/config/SerurityConfig.java
@@ -1,16 +1,24 @@
 package com.wenhao.springsecurity.springsecuritydemo.config;
 
+import com.wenhao.springsecurity.springsecuritydemo.enity.Permission;
 import com.wenhao.springsecurity.springsecuritydemo.haddle.MyAuthenticationFailureHandler;
 import com.wenhao.springsecurity.springsecuritydemo.haddle.MyAuthenticationSuccessHandler;
+import com.wenhao.springsecurity.springsecuritydemo.mapper.PermissionMapper;
+import com.wenhao.springsecurity.springsecuritydemo.mapper.UserMapper;
 import com.wenhao.springsecurity.springsecuritydemo.security.MyUserDetailsService;
+import com.wenhao.springsecurity.springsecuritydemo.until.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @EnableWebSecurity
@@ -25,25 +33,52 @@ public class SerurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService userDetailsService;
 
+    @Autowired
+    private PermissionMapper permissionMapper;
+
     //配置认证用户信息和权限
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //auth.inMemoryAuthentication().withUser("admin").password("123456").authorities("showOrder", "addOrder", "updateOrder", "deleteOrder");
         //auth.inMemoryAuthentication().withUser("admin1").password("123456").authorities("showOrder", "addOrder");
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return MD5Util.encode((String) rawPassword);
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return MD5Util.encode((String) rawPassword).equals(encodedPassword);
+            }
+        });
     }
 
     //配置拦截请求资源
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        /*http.authorizeRequests()
                 .antMatchers("/showOrder").hasAuthority("showOrder")
                 .antMatchers("/addOrder").hasAuthority("addOrder")
                 .antMatchers("/updateOrder").hasAuthority("updateOrder")
                 .antMatchers("/deleteOrder").hasAuthority("deleteOrder")
                 .antMatchers("/login").permitAll()
                 .antMatchers("/**").fullyAuthenticated().and().formLogin().loginPage("/login").successHandler(successHandler)
-                .failureHandler(failureHandler)
+                //.failureHandler(failureHandler)
                 .and()
-                .csrf().disable();
+                .csrf().disable();*/
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry authorizeRequests = http.authorizeRequests();
+        // 查询数据库获取权限列表
+        List<Permission> listPermission = permissionMapper.findAllPermission();
+        for (Permission permission : listPermission) {
+            authorizeRequests.antMatchers(permission.getUrl()).hasAuthority(permission.getPermTag());
+        }
+        authorizeRequests.antMatchers("/login")
+                .permitAll()
+                .antMatchers("/**")
+                .fullyAuthenticated().and().formLogin()
+                .loginPage("/login").successHandler(successHandler)
+                .failureHandler(failureHandler)
+                .and().csrf()
+                .disable();
     }
 
     @Bean
--- src/main/java/com/wenhao/springsecurity/springsecuritydemo/mapper/PermissionMapper.java
+++ src/main/java/com/wenhao/springsecurity/springsecuritydemo/mapper/PermissionMapper.java
@@ -2,9 +2,11 @@ package com.wenhao.springsecurity.springsecuritydemo.mapper;
 
 import com.wenhao.springsecurity.springsecuritydemo.enity.Permission;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PermissionMapper {
 
     // 查询苏所有权限
